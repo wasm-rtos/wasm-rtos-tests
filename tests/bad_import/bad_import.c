c27b84fb76927c689ff196bffdfafed162cfa11d
esp_maniac <91507465+espmaniac@users.noreply.github.com>
2026-07-10 19:57:41 +0300

    Format bad_import guest source

--- tests/bad_import/bad_import.c
+++ tests/bad_import/bad_import.c
@@ -1,5 +1,4 @@
-__attribute__((import_module("env"), import_name("missing_required_import")))
-void missing_required_import(void);
+__attribute__((import_module("env"), import_name("missing_required_import"))) void missing_required_import(void);
 
 void app_main(void)
 {
